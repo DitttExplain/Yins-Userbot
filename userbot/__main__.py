4d72723f2688c33f76a0ac9e7f1f8726b93e6775
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-01-25 00:19:04 +0800

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"✨Yins-Userbot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"⚡Yins-Userbot⚡ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
 
 if len(argv) not in (1, 3, 4):
